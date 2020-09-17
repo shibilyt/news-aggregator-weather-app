fff2e685a29c39790c9d986882ed9735f17efe6f
Mohammed Jumail Shibily <shibilyt@gmail.com>
2020-09-17 14:36:09 +0530

    relative time formatting added to news card

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -3,7 +3,7 @@ import Card from "@material-ui/core/Card";
 
 import { makeStyles } from "@material-ui/core/styles";
 import { Typography } from "@material-ui/core";
-import Skeleton from "@material-ui/lab/Skeleton";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -11,7 +11,8 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: 20,
   },
   newsTitle: {
-    fontSize: 25,
+    textDecoration: 'none',
+    color: theme.palette.dark.main
   },
   publicationDetails: {
     display: "flex",
@@ -22,35 +23,52 @@ const useStyles = makeStyles((theme) => ({
     textDecoration: "none",
     color: theme.palette.primary.dark,
   },
-  body:{
-    display: 'flex',
+  body: {
+    display: "flex",
+    justifyContent: 'space-between'
   },
-  image:{
-    maxWidth: 120,
-    maxHeight: 120,
-    objectFit: ''
-  }
+  content: {
+    marginRight: 10
+  },
+  image: {
+    width: 120,
+    height: 120,
+    objectFit: "fill",
+  },
 }));
 
-export default function NewsCard({data}) {
+export default function NewsCard({ data }) {
   const classes = useStyles();
 
   return (
     <Card className={classes.card}>
       <header>
-        <Typography variant="h6">{data.title}</Typography>
+      <a
+              className={classes.newsTitle}
+              href={data.source.url || data.url}
+            >
+        <Typography variant="h6">{data.title}</Typography></a>
         <div className={classes.publicationDetails}>
           <Typography variant="subtitle1">
-            <a className={classes.newsSource} href={data.source.url || data.url}>
+            <a
+              className={classes.newsSource}
+              href={data.source.url || data.url}
+            >
               {data.source.name}
             </a>
           </Typography>
-          <Typography variant="subtitle1">{data.publishedAt}</Typography>
+          <Typography variant="subtitle1">
+            {formatDistanceToNow(new Date(data.publishedAt))}
+          </Typography>
         </div>
       </header>
       <div className={classes.body}>
-        <Typography variant="body1">{data.description}</Typography>
-        <img src={data.image || data.urlToImage} alt="news topic" className={classes.image} />
+        <Typography variant="body1" className={classes.content}>{data.description}</Typography>
+        <img
+          src={data.image || data.urlToImage}
+          alt="news topic"
+          className={classes.image}
+        />
       </div>
     </Card>
   );
